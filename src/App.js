5097df57f55220add592ab16217fef90a320307e
Louby <loubyc2@gmail.com>
2021-03-04 02:23:44 -0300

    some changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import Services from './components/services/Services';
 import DeepCleaning from './components/deepCleaning/DeepCleaning';
 import About from './components/about/About';
+import Contact from './components/contactUs/Contact';
 
 function PageNotFoung() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route exact path='/services' component={Services} />
         <Route exact path='/deep-cleaning' component={DeepCleaning} />
         <Route exact path='/about' component={About} />
+        <Route exact path='/contact-us' component={Contact} />
         <Route component={PageNotFoung} />
       </Switch>
     </Router>
--- src/components/contactUs/Contact.js
+++ src/components/contactUs/Contact.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Contact.css'
 
 function Contact() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const handleName = e => {
+        setName(e.target.value)
+
+    }
+    const handleEmail = e => {
+        setEmail(e.target.value)
+    }
+    const handleMessage = e => {
+        setMessage(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
+
     return (
-        <div>
-            <form action="" method="post">
-                <label htmlFor="">Nom</label>
-            </form>
+        <div className='contactUs p-5'>
+            <div className='contactUs__content'>
+                <div className='contactUs-left'>
+                    <h6>Laissez-nous votre message</h6>
+                    <form className='contact-form form-control' onSubmit={() => handleSubmit} action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfz76GuxyLyRStx0UD1azYscCANzIsqHKsM0_eAToWIi781KA/formResponse" target='_blanck' method='post'>
+                        <div >
+                            <label className='form-label' htmlFor="">Entrez votre nom</label>
+                            <input className='form-control' type="text" name="entry.1479319914" value={name} onChange={handleName} />
+                        </div>
+                        <div>
+                            <label className='form-label' htmlFor="">Entrez votre adresse email</label>
+                            <input className='form-control' type="email" name="entry.1473153097" value={email} onChange={handleEmail} />
+                        </div>
+                        <div>
+                            <textarea className='form-control' name="entry.1173495417" id="" placeholder='Message' value={message} onChange={handleMessage}></textarea>
+                        </div>
+                        <button className='btn btn-success' type='submit'>Envoyer</button>
+
+                    </form>
+
+                </div>
+                <div className='contactUs-right'>
+                    <p>Phone: <br /> <span>000000</span></p>
+                    <p>Mail: <br /> <span>abc@domaine.com</span></p>
+                    <p>Direction: <br /> <span>street 234</span></p>
+                </div>
+            </div>
+
         </div>
     )
 }
