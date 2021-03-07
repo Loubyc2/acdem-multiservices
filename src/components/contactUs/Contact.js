6bc67b917d68376c7cc617ca62b11e6003ab8bff
Louby <loubyc2@gmail.com>
2021-03-07 20:09:16 -0300

    other commit

--- src/components/contactUs/Contact.js
+++ src/components/contactUs/Contact.js
@@ -11,6 +11,7 @@ import * as HiIcons from 'react-icons/hi'
 function Contact() {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
     const [message, setMessage] = useState('')
 
     const handleName = e => {
@@ -23,6 +24,9 @@ function Contact() {
     const handleMessage = e => {
         setMessage(e.target.value)
     }
+    const handlePhone = e => {
+        setPhone(e.target.value)
+    }
 
 
 
@@ -33,17 +37,17 @@ function Contact() {
             </div>
             <h2>Laissez-nous votre message</h2>
             <div className="contactUs-line"></div>
-            <div className='contactUs__content p-3'>
+            <div className='contactUs__content px-5 pb-5 pt-4'>
                 <div className='contactUs-left'>
-
-                    <form className='contact-form form-control  rounded' onSubmit={e => e.preventDefault} action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfz76GuxyLyRStx0UD1azYscCANzIsqHKsM0_eAToWIi781KA/formResponse" target='_blanck' method='post'>
+                    <form className='contact-form form-control py-3 rounded' onSubmit={e => e.preventDefault} action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfz76GuxyLyRStx0UD1azYscCANzIsqHKsM0_eAToWIi781KA/formResponse" target='_blanck' method='post'>
                         <div >
-                            <label className='form-label' htmlFor="">Votre Nom</label>
-                            <input className='form-control' type="text" name="entry.1479319914" value={name} onChange={handleName} />
+                            <input className='form-control' type="text" name="entry.1479319914" value={name} onChange={handleName} placeholder='Nom' />
                         </div>
                         <div>
-                            <label className='form-label' htmlFor="">Votre Email</label>
-                            <input className='form-control' type="email" name="entry.1473153097" value={email} onChange={handleEmail} />
+                            <input className='form-control' type="email" name="entry.1473153097" value={email} onChange={handleEmail} placeholder='Email' />
+                        </div>
+                        <div>
+                            <input className='form-control' type="text" name="entry.1473153097" value={phone} onChange={handlePhone} placeholder='Tel' />
                         </div>
                         <div>
                             <textarea className='form-control' name="entry.1173495417" id="" placeholder='Message' value={message} onChange={handleMessage}></textarea>
@@ -53,11 +57,7 @@ function Contact() {
                     </form>
 
                 </div>
-                <div className='contactUs-right'>
-                    <p> <strong><HiIcons.HiPhone /></strong>  <span>  +123 8989 8989</span></p>
-                    <p><strong><HiIcons.HiMail /></strong> <span>tmanoucheca@gmail.com</span></p>
-                    <p><strong><HiIcons.HiLocationMarker /></strong>  <span>street 234</span></p>
-                </div>
+
             </div>
             <Footer />
         </div>
